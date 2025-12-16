4c4b6c50856a6610aae509115b04c2550d848744
Hezik <hezik19800388562@outlook.com>
2025-12-16 16:07:17 +0800

    main.py

--- main.py
+++ main.py
@@ -1,40 +1,45 @@
-import numpy as np
-import scipy.io.wavfile
+import argparse
 import os
-from dsp import mfcc 
+from function import (
+    load_esc50,
+    features_extract,
+    evaluate,
+)
 
 
-def features_extract(data_path, frame_time=0.025, hop_time=0.010, savepath="./features"):
-    """ 
-    提取所有对比数据集的MFCC特征并保存
-    data_path:  ESC-50-master数据集位置 
-    frame_time: 帧长的一段时间
-    hop_time:   帧移的一段时间
-    savepath:   npy矩阵保存路径
-    """
-    audio_data = []
-    for audio in reversed(os.listdir(data_path)):
-        if audio[0] == '5':
-            break
-        audio_data.append(audio)
+def main(args):
+    savepath = os.path.join("./features", f"frame_time={args.frame_time}_hop_time={args.hop_time}")
+    audio_data, test_data, all_data = load_esc50()
 
-    for audio in audio_data:
-        audio_path = os.path.join(data_path, audio)
-        sr, signal = scipy.io.wavfile.read(audio_path)
-        frame_len = int(frame_time * sr)
-        hop_len = int(hop_time * sr)
-        feature = mfcc(signal, sr, frame_len, hop_len)
-        norm_feature = (feature - np.mean(feature, axis=0) + 1e-8)
-        np.save(os.path.join(savepath, audio[:-3]+"npy"), norm_feature)
-        print(f"audio: {audio}, MFCC feature computation finished, save to {os.path.join(savepath, audio[:-3]+'npy')}")
-    return True
+    # 特征提取存储(如果还没有保存)
+    if not os.path.exists(savepath):
+        os.mkdir(savepath)
+        features_extract(
+            audio_data=all_data, 
+            savepath=savepath,
+            frame_time=args.frame_time,
+            hop_time=args.hop_time,
+        )
 
-
-def main():
-    features_extract("./data/ESC-50-master/audio")
+    # 相似mfcc特征阵计算获取
+    if len(os.listdir(savepath))==2000:
+        evaluate(
+            test_data, 
+            audio_data, 
+            args.frame_time, 
+            args.hop_time, 
+            k=5, 
+            sim_mode=args.sim_mode,
+        )   
 
 
 if __name__ == "__main__":
-    # print(fft_recursive(np.array([1, 2, 3, 4])))
-    # print(fft_loop(np.array([1, 2, 3, 4])))
-    main()
+    # 参数设置
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--data_path", type=str, default="./data/ESC-50-master/audio")
+    parser.add_argument("--frame_time", type=float, default=0.025)
+    parser.add_argument("--hop_time", type=float, default=0.010)
+    parser.add_argument("--sim_mode", type=str, default="DTW", help="sim mode")
+    args = parser.parse_args()
+    main(args)
+
