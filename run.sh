4abd8297c32bc68b9d9073d379452470b07adac4
Hezik <hezik19800388562@outlook.com>
2025-12-17 21:48:22 +0800

    sun shell

--- run.sh
+++ run.sh
@@ -2,8 +2,8 @@
 # test   hop_time = [0.020, 0.010, 0.005]
 
 CUDA_VISIABLE_DEVICES=0 python main.py \
-    --test_mode hit \
-    --frame_time 0.015 \
-    --hop_time 0.005 \
+    --test_mode score \
+    --frame_time 0.040 \
+    --hop_time 0.020 \
     --k 10 \
     --sim_mode DTW
