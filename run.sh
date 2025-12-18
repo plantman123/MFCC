2c3e55c416ebefd11eca1177cd6516643b9635f0
Hezik <hezik19800388562@outlook.com>
2025-12-18 10:03:58 +0800

    README

--- run.sh
+++
@@ -1,9 +0,0 @@
-# test frame_time = [0.040, 0.025, 0.015]
-# test   hop_time = [0.020, 0.010, 0.005]
-
-CUDA_VISIABLE_DEVICES=0 python main.py \
-    --test_mode score \
-    --frame_time 0.040 \
-    --hop_time 0.020 \
-    --k 10 \
-    --sim_mode DTW
